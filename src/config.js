fd2a7701d50e3e3178d8cfdb222b4354f2df8d28
CyberWarrior1231 <thestaralwayad@gmail.com>
2026-02-19 11:45:44 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,5 +10,6 @@ module.exports = {
   defaultWarningLimit: Number(process.env.WARNING_LIMIT || 3),
   captchaTimeoutSeconds: Number(process.env.CAPTCHA_TIMEOUT_SECONDS || 120),
   floodWindowMs: Number(process.env.FLOOD_WINDOW_MS || 8000),
-  floodMessageLimit: Number(process.env.FLOOD_MESSAGE_LIMIT || 6)
+  floodMessageLimit: Number(process.env.FLOOD_MESSAGE_LIMIT || 6),
+  ownerId: Number(process.env.OWNER_ID || 0)
 };
